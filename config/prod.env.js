18cea7afbfadf3d7c48298cd2c5a4a146edaf0bf
Max Dobeck <msdobeck@gmail.com>
2018-07-03 22:48:39 -0700

    moved configs from dev.env to prod.env.

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  TEST_API: '"https://shielded-stream-75107.herokuapp.com/"',
+  MY_TEST: '"My big test case"'
 }
