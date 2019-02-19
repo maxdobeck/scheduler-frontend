a3e0a643644a7ec959394b953564b0d6703d2aaa
Max Dobeck <msdobeck@gmail.com>
2019-02-18 17:48:24 -0800

    Preparing to add shift view.

---
+++ src/components/shifts/Shift.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  Shifts
+</div>
+</template>
+
+<script>
+// import { mapGetters } from 'vuex'
+// let api // Need to find a way to turn all this into a function
+// if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'dev') {
+//   api = process.env.DEV_API
+// } else if (process.env.NODE_ENV === 'test') {
+//   api = process.env.TEST_API
+// } else if (process.env.NODE_ENV === 'production') {
+//   api = process.env.PROD_API
+// }
+// const apiSchedules = api + 'shifts'
+
+export default {
+}
+</script>
+
+<style scoped>
+</style>
