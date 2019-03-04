69d98225a2a49b99e33d1258c28f26ef80b82142
Max Dobeck <msdobeck@gmail.com>
2019-03-03 19:29:16 -0800

    Added Shifts to list all for CRUD control.

---
+++ src/components/shifts/Shifts.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <h1>Shifts</h1>
+  <div>{{ shifts }}</div>
+
+</div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  data () {
+    return {
+      shiftURI: '',
+      shift: ''
+    }
+  },
+  name: 'Shifts',
+  watch: {
+    '$route': 'getShifts'
+  },
+  methods: {
+    async getShifts () {
+      await this.$store.dispatch('getShifts', this.$route.params.id)
+    }
+  },
+  computed: {
+    ...mapGetters({
+      shifts: 'shifts'
+    })
+  },
+  mounted () {
+    this.getShifts()
+  }
+}
+</script>
+
+<style scoped>
+</style>
